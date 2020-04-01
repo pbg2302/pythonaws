54218410f90560d0762a961c3f4ad4420ce585e4
pbg2302 <pbg2302@gmail.com>
2020-04-01 22:39:18 +0100

    values removed

--- boto.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-aws_con = boto3.session.Session(profile_name="default")
-
-iam = aws_con.client('iam')
-
-response = iam.list_users()
-for users in response['Users']:
-    print(users['UserName'])
